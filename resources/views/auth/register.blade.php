c236f7edc8963bf43cfd7f0cfae9f748ec77b2c9
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2025-04-01 09:46:43 +0300

    nam nam

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reģistrēties</title>
+</head>
+<body>
+    <h1>Reģistrēties</h1>
+
+    <form method="POST">
+        @csrf
+
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+          
+        <label>
+            First Name<br>
+            <input name="first_name" required/>
+        </label><br><br>
+
+        <label>
+            Last Name<br>
+            <input name="last_name" required/>
+        </label><br><br>
+
+        <label>
+            Email<br>
+            <input name="email" type="email" required/>
+        </label><br><br>
+
+        <label>
+            Password<br>
+            <input name="password" type="password" required/>
+        </label><br><br>
+
+        <label>
+            Confirm Password<br>
+            <input name="password_confirmation" type="password" required/>
+        </label><br><br>
+
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
